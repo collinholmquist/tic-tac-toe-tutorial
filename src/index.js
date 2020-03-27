037f4183d63691cbd4a950b1bdca9f21e0b6e48d
Collin Holmquist <collinholmquist@gmail.com>
2020-03-27 15:04:13 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
     render() {
       return (
-        <button className="square">
-          {this.props.value}
-        </button>
+        <button className="square" 
+        onClick={() => this.props.onClick()}
+        >
+          {this.props.value} 
+        </button> //value passed from board
       );
     }
   }
   
   class Board extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+    
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+
+    }
+
     renderSquare(i) {
-      return <Square />;
+      return (
+      <Square //passing Square value and onClick
+      value ={this.state.squares[i]}
+      onClick={() => this.handleClick(i)} /> 
+      );
     }
   
     render() {
